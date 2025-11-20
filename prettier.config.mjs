981e5fa57d249b0d2a779c6682371e8b414a95fc
Franklin Shera <fshera96@gmail.com>
2025-11-20 14:18:27 +0300

    setup linting and formating

---
+++ prettier.config.mjs
@@ -0,0 +1,8 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+export default {
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["clsx", "cn"],
+};
